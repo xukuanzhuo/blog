b2a73ca9b26e7266fedd888e4bf7a7810d680fbf
xkz <1060821554@qq.com>
2018-11-08 19:10:08 +0800

    init react with webpack

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Blog](https://github.com/xukuanzhuo/xukuanzhuo.github.io) &middot;
+
+
+### - [You Don't Know JS 系列书籍阅读笔记](https://github.com/xukuanzhuo/xukuanzhuo.github.io/projects/1)
+
+作用域和闭包 |
+---- | ---
+[JavaScript 中的 LHS 和 RHS 查询](https://github.com/xukuanzhuo/xukuanzhuo.github.io/issues/1) |
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
