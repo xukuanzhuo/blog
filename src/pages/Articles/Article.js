f5f681fa04df15d615f6e36c036295c564c91c1e
xkz <1060821554@qq.com>
2019-01-14 14:15:44 +0800

    article page skeleton

--- src/pages/Articles/Article.js
+++ src/pages/Articles/Article.js
@@ -3,8 +3,21 @@ import CSSModules from 'react-css-modules'
 import styles from './Article.css'
 import { getBlogIssue } from 'src/api/issues.js'
 import showdown from 'showdown'
+import Skeleton from 'react-loading-skeleton'
 
 const convert = new showdown.Converter()
+
+function ArticleSkeleton () {
+  return (
+    <article>
+      <header>
+        <Skeleton height={ 30 } />
+        <Skeleton width={ 200 } />
+      </header>
+      <Skeleton count={ 5 } />
+    </article>
+  )
+}
 
 class Article extends Component {
   constructor (props) {
@@ -33,17 +46,19 @@ class Article extends Component {
 
     return (
       <div styleName="articlePage">
-        <article>
-          <header>
-            <h1 styleName="articleHeaderTitle">{ issue.title }</h1>
-            <div styleName="articleHeaderInfos">
-              <span>{ issue.user.login }</span>
-              <span> { issue.updatedAt }</span>
-            </div>
-          </header>
-          <section styleName="articleSubContent" dangerouslySetInnerHTML={{ __html: convert.makeHtml(issue.body) }}>
-          </section>
-        </article>
+        { issue.title ?
+          <article>
+            <header>
+              <h1 styleName="articleHeaderTitle">{ issue.title  }</h1>
+              <div styleName="articleHeaderInfos">
+                <span>{ issue.user.login }</span>
+                <span> { issue.updatedAt }</span>
+              </div>
+            </header>
+            <section styleName="articleSubContent" dangerouslySetInnerHTML={{ __html: convert.makeHtml(issue.body) }} />
+          </article>
+          : <ArticleSkeleton />
+        }
       </div>
     )
   }
